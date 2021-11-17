04f6d66b0ebd078074910d1a4ec8c59f61daeba2
AdrianLB08 <91706214+AdrianLB08@users.noreply.github.com>
2021-11-16 22:23:47 -0600

    Funcion6
    Calculo

---
+++ Funcion6.m
@@ -0,0 +1,24 @@
+%octave.script
+%Escuela:  Tecnologico de estudios superiores de jilotepec 
+%Especialidad: Ingenieria en Sistemas Computacionales 
+%Title          :Funcion inyectiva,suprayectiva y biyectiva
+%Description    :Script para recordar inyectiva,suprayectiva y biyectiva
+%Author         :Adrian Lovera Bombela 
+%Date           :202123498
+%Version        :1
+%Usage          :octave> /path/inyectivasuprayectivaybiyectiva
+%Notes          :Requiere aplicación octave, usar su linea de comandos 
+%Fecha          :16/11/2021
+
+%Ejecución de función 6
+clear
+pkg load symbolic
+syms x
+x=[-20:1:20]
+fx=((x.^4)(+1)./(x.^3));
+plot(x,fx)
+disp ("Esta funcion es de tipo biyectiva");
+grid on
+ylabel('fx')
+xlabel('x')
+title('Funcion6, como resultado es una función biyectiva)');
